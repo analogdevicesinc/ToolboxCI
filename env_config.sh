a06ffa5d82ae7f991dfe5d7392c4084cc6717903
Stanca Pop <stanca.pop@analog.com>
2025-05-26 10:03:00 +0300

    env: Update Vivado 2024.2

--- env_config.sh
+++ env_config.sh
@@ -4,7 +4,7 @@
 export req_matlab_v="R2023b"
 
 # Set the required VIVADO version
-export req_vivado_v="2023.2"
+export req_vivado_v="2024.2"
 
 # Define the full path to settings64.sh, using the Vivado version
 export vivado_settings_path="/opt/Xilinx/Vivado/$req_vivado_v/settings64.sh"
